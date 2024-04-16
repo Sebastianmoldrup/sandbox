900caff6ddf792f2644ad4cc585992955106a28b
Sebastian Møldrup <sebastianmoldrup@Sebastians-Air.home>
2024-04-17 00:53:14 +0200

    added image

--- app/admin/image/page.tsx
+++ app/admin/image/page.tsx
@@ -2,20 +2,23 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
+import { supabase } from '../../utils/supabase/client';
 
 export default function Page() {
 
     const [img, setImg] = useState<string | null>(null);
     console.log(img);
 
-    function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    async function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
         const imgFile = event.target.files[0];
+        console.log('file:', imgFile);
         const imgUrl = URL.createObjectURL(imgFile);
+        console.log('url:', imgUrl);
         setImg(imgUrl);
-
+    
         const { data, error } = await supabase.storage
-  .from('avatars')
-  .upload('public/avatar1.png', avatarFile)
+        .from('images')
+        .upload(imgFile.name, imgFile); // upload the file to the 'images' bucket with its original name
     }
 
     return <div className="h-screen grid place-content-center gap-y-4">
